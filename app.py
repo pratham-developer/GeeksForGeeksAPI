0ab50fc529ee241802d5aeed9d7ad20d730028ca
Pratham Khanduja <73526443+pratham-developer@users.noreply.github.com>
2025-03-25 19:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from modules.scrap import scrap
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 api = Api(app)
 
 class geeksforgeeksAPI(Resource):
     def get(self, username):
         scrapper = scrap(username)
-        return scrapper.fetchResponse() 
-
+        return scrapper.fetchResponse()
 
 api.add_resource(geeksforgeeksAPI, "/<string:username>")
 
